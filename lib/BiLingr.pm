9a24555fec1f9397de0d9b63768db0669ce6e708
hiratara <hiratara@cpan.org>
2009-04-19 08:43:01 +0900

    initial commit

---
+++ lib/BiLingr.pm
@@ -0,0 +1,33 @@
+package BiLingr;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+BiLingr -
+
+=head1 SYNOPSIS
+
+  use BiLingr;
+
+=head1 DESCRIPTION
+
+BiLingr is
+
+=head1 AUTHOR
+
+hiratara E<lt>hiratara@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
